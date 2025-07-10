9e6be3a312506b6ed0033c40a0e90e8b620fdde6
pc <pc@DESKTOP-86M3HVC>
2025-07-10 12:49:04 +0300

    HealthController edited

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinexaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Finexa API is alive");
+        }
+    }
+}
